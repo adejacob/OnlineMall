77ffca42eeadd368aa6e737a0ed368ab73b07f04
adejacob <adejacob000@gmail.com>
2019-11-05 12:24:26 +0100

    Add All Files

---
+++ connection.php
@@ -0,0 +1,4 @@
+<?php
+$con=mysqli_connect("localhost","root","Ponnysword@1","store") or die(mysqli_error($con));
+//$con=mysqli_connect("localhost","root","","store") or die(mysqli_error($con));
+?>
